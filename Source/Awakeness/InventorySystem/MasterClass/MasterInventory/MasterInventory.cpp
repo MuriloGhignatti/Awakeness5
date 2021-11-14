de3e6c0d4096a5da8ede97023776141e5935bac0
Murilo Ghignatti <muriloghignatti@gmail.com>
2021-11-14 15:00:02 -0300

    Created basic MasterInventory (Copied From Previous Version)

--- Source/Awakeness/InventorySystem/MasterClass/MasterInventory/MasterInventory.cpp
+++ Source/Awakeness/InventorySystem/MasterClass/MasterInventory/MasterInventory.cpp
@@ -1,21 +1,18 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MasterInventory.h"
-
+﻿#include "MasterInventory.h"
 
 // Sets default values
 AMasterInventory::AMasterInventory()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	InitSlots();
 }
 
 // Called when the game starts or when spawned
 void AMasterInventory::BeginPlay()
 {
+	InitSlots();
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -24,3 +21,142 @@ void AMasterInventory::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+bool AMasterInventory::IsSlotEmpty(const int32 Index)
+{
+	return Slots[Index].ItemClass == nullptr;
+}
+
+FItemInfo AMasterInventory::GetItemInfoAtIndex(const int32 Index, int32& Amount, bool& IsEmpty)
+{
+	IsEmpty = false;
+	if (IsSlotEmpty(Index))
+	{
+		IsEmpty = true;
+		return FItemInfo{};
+	}
+	Amount = Slots[Index].Amount;
+	return Slots[Index].ItemClass->ItemInfo;
+}
+
+int32 AMasterInventory::SearchEmptySlot()
+{
+	for (size_t i = 0; i < Slots.Num(); ++i)
+		if (IsSlotEmpty(i))
+			return i;
+
+	return INDEX_NONE;
+}
+
+int32 AMasterInventory::SearchFreeStack(const TSubclassOf<class AMasterItem> ItemClass)
+{
+	for (short i = 0; i < Slots.Num(); i++)
+		if (!IsSlotEmpty(i) && Slots[i].ItemClass->Id == ItemClass.GetDefaultObject()->Id && Slots[i].Amount <
+			MaxStackSize)
+			return i;
+
+	return INDEX_NONE;
+}
+
+int32 AMasterInventory::AddItem(const TSubclassOf<class AMasterItem> ItemClass, const int32 Amount)
+{
+	int FoundIndex = INDEX_NONE;
+	if (ItemClass.GetDefaultObject()->ItemInfo.Stackable)
+	{
+		if ((FoundIndex = SearchFreeStack(ItemClass)) != INDEX_NONE)
+		{
+			if ((Slots[FoundIndex].Amount + Amount) > MaxStackSize)
+			{
+				Slots[FoundIndex].Amount = MaxStackSize;
+				UpdateSlotWidget(FoundIndex);
+				return AddItem(ItemClass, Amount - MaxStackSize);
+			}
+			Slots[FoundIndex].Amount += Amount;
+			UpdateSlotWidget(FoundIndex);
+			return 0;
+		}
+
+		if ((FoundIndex = SearchEmptySlot()) != INDEX_NONE)
+		{
+			if (Amount > MaxStackSize)
+			{
+				Slots[FoundIndex] = FInventorySlot{NewObject<AMasterItem>(ItemClass, ItemClass), MaxStackSize};
+				UpdateSlotWidget(FoundIndex);
+				return AddItem(ItemClass, Amount - MaxStackSize);
+			}
+			Slots[FoundIndex] = FInventorySlot{NewObject<AMasterItem>(ItemClass, ItemClass), Amount};
+			UpdateSlotWidget(FoundIndex);
+			return 0;
+		}
+		return -1;
+	}
+	if ((FoundIndex = SearchEmptySlot()) != INDEX_NONE)
+	{
+		Slots[FoundIndex] = FInventorySlot{NewObject<AMasterItem>(ItemClass, ItemClass), 1};
+		UpdateSlotWidget(FoundIndex);
+		if (Amount > 1)
+			return AddItem(ItemClass, Amount - 1);
+		return 0;
+	}
+	return -1;
+}
+
+int32 AMasterInventory::GetAmountAtIndex(const int32 Index)
+{
+	return Slots[Index].Amount;
+}
+
+bool AMasterInventory::RemoveItemAtIndex(const int32 Index, const int32 Amount)
+{
+	if (Slots[Index].Amount <= Amount)
+	{
+		Slots[Index].ItemClass = nullptr;
+		UpdateSlotWidget(Index);
+		return true;
+	}
+	if (Slots[Index].Amount == Amount)
+	{
+		Slots[Index].ItemClass = nullptr;
+		UpdateSlotWidget(Index);
+		return true;
+	}
+	Slots[Index].Amount -= Amount;
+	UpdateSlotWidget(Index);
+	return true;
+}
+
+bool AMasterInventory::SwapSlots(const int32 Index1, const int32 Index2)
+{
+	FInventorySlot Holder = Slots[Index1];
+	Slots[Index1] = Slots[Index2];
+	UpdateSlotWidget(Index1);
+	Slots[Index2] = Holder;
+	UpdateSlotWidget(Index2);
+	return true;
+}
+
+bool AMasterInventory::SplitStack(const int32 Index, const int32 Amount)
+{
+	if (Slots[Index].Amount > Amount && Slots[Index].ItemClass->ItemInfo.Stackable)
+	{
+		int Found = SearchEmptySlot();
+		if (Found != INDEX_NONE)
+		{
+			Slots[Index].Amount -= Amount;
+			Slots[Found] = FInventorySlot{Slots[Index].ItemClass, Amount};
+			UpdateSlotWidget(Index);
+			return true;
+		}
+		return false;
+	}
+	return false;
+}
+
+bool AMasterInventory::UseItemAtIndex(const int32 Index, const int32 Amount)
+{
+	if (IsSlotEmpty(Index))
+		return false;
+
+	Slots[Index].ItemClass->OnUse(Amount);
+	RemoveItemAtIndex(Index, Amount);
+	return true;
+}
--- Source/Awakeness/InventorySystem/MasterClass/MasterInventory/MasterInventory.h
+++ Source/Awakeness/InventorySystem/MasterClass/MasterInventory/MasterInventory.h
@@ -4,22 +4,95 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "../../Structs/InventorySlot.h"
 #include "MasterInventory.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class AWAKENESS_API AMasterInventory : public AActor
 {
 	GENERATED_BODY()
-
-public:
+	
+public:	
 	// Sets default values for this actor's properties
 	AMasterInventory();
+
+	UPROPERTY(BlueprintReadWrite, BlueprintGetter=GetSlotsInited)
+	bool SlotsInited = false;
+	
+	UPROPERTY(BlueprintReadWrite, Meta = (ExposeOnSpawn = "true"))
+	int32 AmountOfSlots = 21;
+
+	UPROPERTY(BlueprintReadWrite, Meta = (ExposeOnSpawn = "true"))
+	int32 AmountOfSlotsInHotbar = 5;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = "true"))
+	int32 MaxStackSize = 9999;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TArray<FInventorySlot> Slots;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsSlotEmpty(const int32 Index);
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FItemInfo GetItemInfoAtIndex(const int32 Index, int32& Amount, bool& IsEmpty);
+	UFUNCTION(BlueprintCallable)
+	int32 SearchEmptySlot();
+	UFUNCTION(BlueprintCallable)
+	int32 SearchFreeStack(TSubclassOf<class AMasterItem> ItemClass);
+	UFUNCTION(BlueprintCallable)
+	int32 AddItem(TSubclassOf<class AMasterItem> ItemClass, const int32 Amount);
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	int32 GetAmountAtIndex(const int32 Index);
+	UFUNCTION(BlueprintCallable)
+	bool RemoveItemAtIndex(const int32 Index, const int32 Amount);
+	UFUNCTION(BlueprintCallable)
+	bool SwapSlots(const int32 Index1, const int32 Index2);
+	UFUNCTION(BlueprintCallable)
+	bool SplitStack(const int32 Index, const int32 Amount);
+	UFUNCTION(BlueprintCallable)
+	bool UseItemAtIndex(const int32 Index, const int32 Amount);
+
+	UFUNCTION(BlueprintImplementableEvent)
+    void UpdateSlotWidget(int32 Index);
+
+	UFUNCTION(BlueprintGetter)
+	TArray<FInventorySlot> GetSlots()
+	{
+		if(!SlotsInited)
+		{
+			InitSlots();
+		}
+		return Slots;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void InitSlots()
+	{
+		if(!SlotsInited)
+		{
+			Slots.Init(FInventorySlot{nullptr, 0}, (AmountOfSlots + AmountOfSlotsInHotbar) + 1);
+			SlotsInited = true;
+		}
+	}
+	
+	UFUNCTION(BlueprintGetter)
+	bool GetSlotsInited()
+	{
+		return SlotsInited;
+	}
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetSlotsSize()
+	{
+		return Slots.Num();
+	}
+	
 };
